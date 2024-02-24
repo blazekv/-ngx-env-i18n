81efe5ba62ebb8c18872da8cc9805a699fb3a44f
blazekv <vladimir.blazek@quanti.cz>
2024-02-24 14:28:36 +0100

    i18n extraction with ngx-env/builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngx-env-test';
+  title = $localize`ngx-env-test`;
 }
